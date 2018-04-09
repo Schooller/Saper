fd0c2f59502d75b9be9999b6818aedadf7a33bf5
Yury Moldavanov <yury.moldavanov@zimad.com>
2018-04-09 14:13:53 +0300

    C# console app implementation

---
+++ SaperWinConsole/Saper/Minesweeper.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace Saper
+{
+    public struct Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public enum FieldState
+    {
+        Closed,
+        MarkedAsBomb,
+        Explosed,
+        Opened,
+    }
+
+    public class Cell
+    {
+        public FieldState State { get; set; }
+        public bool HasBomb { get; set; }
+        public int NeighborBobmsCount { get; set; }
+    }
+
+    public class Field
+    {
+        Field() { }
+        List<Cell> _Cells = new List<Cell>();
+        public int Colunms { get; private set; }
+        public int Rows { get; private set; }
+
+        public List<Coordinate> Neighbors(Coordinate coordinate)
+        {
+            List<Coordinate> list = new List<Coordinate>();
+            if (coordinate.X > 0)
+                list.Add(new Coordinate { X = coordinate.X - 1, Y = coordinate.Y });
+            if (coordinate.X < (Colunms - 1))
+                list.Add(new Coordinate { X = coordinate.X + 1, Y = coordinate.Y });
+            if (coordinate.Y > 0)
+                list.Add(new Coordinate { X = coordinate.X, Y = coordinate.Y - 1 });
+            if (coordinate.Y < (Rows - 1) )
+                list.Add(new Coordinate { X = coordinate.X, Y = coordinate.Y + 1 });
+            if (coordinate.X > 0 && coordinate.Y > 0)
+                list.Add(new Coordinate { X = coordinate.X - 1, Y = coordinate.Y - 1 });
+            if (coordinate.X > 0 && coordinate.Y < (Rows - 1))
+                list.Add(new Coordinate { X = coordinate.X - 1, Y = coordinate.Y + 1 });
+            if (coordinate.X < (Colunms - 1) && coordinate.Y > 0 )
+                list.Add(new Coordinate { X = coordinate.X + 1, Y = coordinate.Y - 1 });
+            if (coordinate.X < (Colunms - 1) && coordinate.Y < (Rows - 1) )
+                list.Add(new Coordinate { X = coordinate.X + 1, Y = coordinate.Y + 1 });
+            return list;
+        }
+
+
+        public int ClosedCellsNumber
+        {
+            get
+            {
+                return _Cells.Count((cell) => 
+                {
+                    return cell.State == FieldState.Closed; 
+                });
+            }
+        }
+
+        public int MarkedAsBombCellsNumber
+        {
+            get
+            {
+                return _Cells.Count((cell) =>
+                {
+                    return cell.State == FieldState.MarkedAsBomb;
+                });
+            }
+        }
+
+
+        public static Field Create(Options options)
+        {
+            return Create(options.Columns, options.Rows, options.Bombs);
+        }
+
+        public static Field Create(int colunms, int rows, int bombs)
+        {
+            if (colunms < 1 || rows < 1)
+                throw new ArgumentException();
+            Field instance = new Field
+            {
+                Colunms = colunms,
+                Rows = rows
+            };
+            instance.createCells();
+            instance.putBombs(bombs);
+            instance.calcNeighborBombs();
+            return instance;
+        }
+
+        void calcNeighborBombs()
+        {
+            for(int i = 0; i < _Cells.Count; i++)
+            {
+                fillNeighborBombsNumber(index2Coordinate(i));
+            }
+        }
+
+        void fillNeighborBombsNumber(Coordinate coordinate)
+        {
+            Cell(coordinate).NeighborBobmsCount = Neighbors(coordinate).Count((neighbor)=> 
+            {
+                return Cell(neighbor).HasBomb;
+            });
+        }
+
+        void putBombs(int bombs)
+        {
+            if (bombs < 1 || bombs >= Colunms * Rows)
+                throw new ArgumentException();
+
+            Random rand = new Random(DateTime.Now.Millisecond);
+
+            do
+            {
+                int index = rand.Next(_Cells.Count);
+                if (!_Cells[index].HasBomb)
+                {
+                    _Cells[index].HasBomb = true;
+                    bombs--;
+                }
+            }
+            while (bombs > 0);
+        }
+
+        void createCells()
+        {
+            _Cells.Clear();
+            for (int y = 0; y < Rows; y++)
+            {
+                for (int x = 0; x < Colunms; x++)
+                {
+                    _Cells.Add(new Cell());
+                }
+            }
+        }
+
+        public Cell Cell(int x, int y)
+        {
+            return _Cells[coordinate2Index(new Coordinate { X = x, Y = y })];
+        }
+
+        public Cell Cell(Coordinate coordinate)
+        {
+            if (coordinate.X < 0 || coordinate.X >= Colunms || coordinate.Y < 0 || coordinate.Y >= Rows)
+                throw new ArgumentException("invalid coordinates");
+            return _Cells[coordinate2Index(coordinate)];
+        }
+
+        int coordinate2Index(Coordinate coordinate)
+        {
+            return coordinate.X * Colunms + coordinate.Y;
+        }
+        Coordinate index2Coordinate(int index)
+        {
+            return new Coordinate { X = index % Colunms, Y = index / Colunms };
+        }
+    }
+
+    public class Options
+    {
+        public int Columns { get; set; }
+        public int Rows { get; set; }
+        public int Bombs { get; set; }
+        public static Options Beginner
+        {
+            get
+            {
+                return new Options { Bombs = 10, Columns = 10, Rows = 10 };
+            }
+        }
+        public static Options Intermediate
+        {
+            get
+            {
+                return new Options { Bombs = 40, Columns = 16, Rows = 16 };
+            }
+        }
+        public static Options Expert
+        {
+            get
+            {
+                return new Options { Bombs = 99, Columns = 31, Rows = 16 };
+            }
+        }
+    }
+
+    public enum GameState
+    {
+        Inited,
+        Playing,
+        Win,
+        Lost
+    }
+
+    public class MinesweeperGame
+    {
+        public MinesweeperGame()
+        {
+            Options = Options.Beginner;
+            New();
+        }
+
+        public event Action<bool> GameOver = null;
+        public event Action<int, int> CellStateChange = null;
+        public event Action<int> Tick = null;
+
+        Field _Field = null;
+        
+        public Options Options { get; private set; }
+        public GameState State { get; private set; }
+        int _Ticks = 0;
+        Timer _Timer = null;
+
+        public int Time
+        {
+            get
+            {
+                return _Ticks;
+            }
+        }
+        
+        public void New()
+        {
+            stopTimer();
+            _Field = Field.Create(Options);
+            _Ticks = 0;
+            State = GameState.Inited;
+        }
+
+        public void UpdateOptions(Options options)
+        {
+            if (options.Bombs < 1 || options.Columns < 1 || options.Bombs >= options.Rows * options.Columns)
+                throw new ArgumentException();
+            Options = options;
+            New();
+        }
+
+        public Cell Cell(int x, int y)
+        {
+            return _Field.Cell(x, y);
+        }
+
+        private void TimerTick(Object state)
+        {
+            _Ticks++;
+            if (Tick != null)
+            {
+                Tick.Invoke(Time);
+            }
+        }
+
+        void startTimer()
+        {
+            stopTimer();
+            _Timer = new Timer(TimerTick, null, 0, 1000);
+        }
+
+        void stopTimer()
+        {
+            if (_Timer != null)
+            {
+                _Timer.Dispose();
+                _Timer = null;
+            }
+        }
+
+        public void MarkAsBomb(int x, int y)
+        {
+            Cell cell = getClickedCell(x, y);
+            if (cell == null || cell.State != FieldState.Closed)
+                return;
+
+            cell.State = FieldState.MarkedAsBomb;
+            onCellStateChange(x, y);
+
+            checkGameOver();
+        }
+
+        public void UnMarkAsBomb(int x, int y)
+        {
+            Cell cell = getClickedCell(x, y);
+            if (cell == null || cell.State != FieldState.MarkedAsBomb)
+                return;
+
+            cell.State = FieldState.Closed;
+            onCellStateChange(x, y);
+        }
+
+        Cell getClickedCell(int x, int y)
+        {
+            if (State == GameState.Win || State == GameState.Lost)
+                return null;
+
+            if (State == GameState.Inited)
+            {
+                State = GameState.Playing;
+                startTimer();
+            }
+            return _Field.Cell(x, y);
+        }
+
+        public void OpenCell(int x, int y)
+        {
+            Cell cell = getClickedCell(x, y);
+            if (cell == null || cell.State != FieldState.Closed)
+                return;
+
+            if (cell.HasBomb)
+            {
+                cell.State = FieldState.Explosed;
+                onCellStateChange(x, y);
+                onGameOver(false);
+            }
+            else
+            {
+                cell.State = FieldState.Opened;
+                onCellStateChange(x, y);
+                if (!checkGameOver() && (cell.NeighborBobmsCount == 0))
+                {
+                    foreach(Coordinate neighbor in _Field.Neighbors(new Coordinate { X = x, Y = y }))
+                    {
+                        OpenCell(neighbor.X, neighbor.Y);
+                    }
+                }
+            }
+        }
+
+        bool checkGameOver()
+        {
+            if (_Field.ClosedCellsNumber + _Field.MarkedAsBombCellsNumber == Options.Bombs)
+            {
+                onGameOver(true);
+                return true;
+            }
+            return false;
+        }
+
+        void onCellStateChange(int x, int y)
+        {
+            if (CellStateChange != null)
+                CellStateChange.Invoke(x, y);
+        }
+
+        void onGameOver(bool isWin)
+        {
+            stopTimer();
+            if (isWin)
+            {
+                State = GameState.Win;
+            }
+            else
+            {
+                State = GameState.Lost;
+            }
+            if (GameOver != null)
+                GameOver.Invoke(isWin);
+        }
+    }
+}
---
+++ SaperWinConsole/Saper/MinesweeperConsoleApp.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saper
+{
+    interface IView
+    {
+        void Draw();
+        bool ProcessUserInput(string input);
+    }
+
+    class OptionsView : IView
+    {
+        int _Columns = 0;
+        int _Rows = 0;
+        int _Bombs = 0;
+        public void Draw()
+        {
+            if (_Columns == 0)
+                Console.Write("Lines (2 - 100):");
+            else if (_Rows == 0)
+                Console.Write("Rows (2 - 100):");
+            else if (_Bombs == 0)
+                Console.Write(String.Format("Bombs (1 - {0}):", _Rows * _Columns - 1));
+        }
+
+        public bool ProcessUserInput(string input)
+        {
+            try
+            {
+                int value = Convert.ToInt32(input);
+                if (value < 2 || value > 100)
+                    throw new Exception();
+                if (_Columns == 0)
+                {
+                    _Columns = value;
+                }
+                else if (_Rows == 0)
+                {
+                    _Rows = value;
+                }
+                else
+                {
+                    _Bombs = value;
+                    setOptions();
+                    ViewManager.Instance.Remove(this);
+                    return true;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid value, try again.");
+                return false;
+            }
+            return false;
+        }
+
+        void setOptions()
+        {
+            MinesweeperConsoleApp.Game.UpdateOptions(new Options { Bombs = _Bombs, Columns = _Columns, Rows = _Rows });
+        }
+    }
+
+    class GameView : IView
+    {
+        public GameView()
+        {
+
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("State:" + MinesweeperConsoleApp.Game.State);
+            Console.WriteLine("Time: " +MinesweeperConsoleApp.Game.Time);
+
+            for (int y = -2; y < MinesweeperConsoleApp.Game.Options.Rows; y++)
+            {
+                Console.Write(((y > -1) ? y.ToString() + "|" : "  "));
+
+                for (int x = 0; x < MinesweeperConsoleApp.Game.Options.Columns; x++)
+                {
+                    if (y == -2)
+                    {
+                        Console.Write(x + " ");
+                    }
+                    else if (y == -1)
+                    {
+                        Console.Write(x == MinesweeperConsoleApp.Game.Options.Columns - 1 ? "_" : "__");
+                    }
+                    else
+                    {
+                        Cell cell = MinesweeperConsoleApp.Game.Cell(x, y);
+                        drawCell(cell);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            if (MinesweeperConsoleApp.Game.State == GameState.Inited || MinesweeperConsoleApp.Game.State == GameState.Playing)
+                Console.WriteLine("Your turn (format is [<action_char> <x> <y>]. <action_char> is 'O' (Open) or 'B' (Mark as bomb / unmark). E.g. 'O 2 3' or 'B 6 7'): ");
+        }
+
+        public void drawCell(Cell cell)
+        {
+            char cellChar = ' ';
+            switch(cell.State)
+            {
+                case FieldState.Closed:
+                    cellChar = 'H';
+                    break;
+                case FieldState.Opened:
+                    cellChar = cell.HasBomb ? 'B' : (cell.NeighborBobmsCount == 0 ? ' ' : cell.NeighborBobmsCount.ToString().ElementAt(0));
+                    break;
+                case FieldState.Explosed:
+                    cellChar = 'X';
+                    break;
+                case FieldState.MarkedAsBomb:
+                    cellChar = 'M';
+                    break;
+            }
+            Console.Write(cellChar + " ");
+        }
+
+        public bool ProcessUserInput(string input)
+        {
+            try
+            {
+                var arr = input.Split(' ');
+
+                int x = Convert.ToInt16(arr[1]);
+                int y = Convert.ToInt16(arr[2]);
+                char action = Convert.ToChar(arr[0].ToUpper());
+                switch(action)
+                {
+                    case 'O':
+                        MinesweeperConsoleApp.Game.OpenCell(x, y);
+                        break;
+                    case 'B':
+                        Cell cell = MinesweeperConsoleApp.Game.Cell(x, y);
+                        if (cell.State == FieldState.Closed)
+                            MinesweeperConsoleApp.Game.MarkAsBomb(x, y);
+                        else if (cell.State == FieldState.MarkedAsBomb)
+                            MinesweeperConsoleApp.Game.UnMarkAsBomb(x, y);
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return true;
+        }
+    }
+
+    class Menu
+    {
+        public void Draw()
+        {
+            Console.WriteLine("[N]ew game o[P]tions [E]xit");
+        }
+
+        public bool ProcessUserInput(string input)
+        {
+            switch(input.ToUpper())
+            {
+                case "P":
+                    ViewManager.Instance.Add(new OptionsView());
+                    return true;
+                case "E":
+                    ViewManager.Instance.CloseAllViews();
+                    return true;
+                case "N":
+                    MinesweeperConsoleApp.Game.New();
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    class ViewManager
+    {
+        ViewManager() { }
+        static ViewManager _Instance;
+        public static ViewManager Instance
+        {
+            get { return _Instance ?? (_Instance = new ViewManager()); }
+        }
+        List<IView> _Viewes = new List<IView>();
+        public void Add(IView view)
+        {
+            _Viewes.Add(view);
+        }
+        public void Remove(IView view)
+        {
+            if (_Viewes.Contains(view))
+                _Viewes.Remove(view);
+        }
+        public IView CurrentView
+        {
+            get
+            {
+                return _Viewes.Count > 0 ? _Viewes.Last() : null;
+            }
+        }
+        public void CloseAllViews()
+        {
+            _Viewes.Clear();
+        }
+    }
+
+    class MinesweeperConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            MinesweeperConsoleApp app = new MinesweeperConsoleApp();
+            while (app.Run())
+            {
+
+            }
+        }
+        static MinesweeperGame _Game = null;
+        public static MinesweeperGame Game
+        {
+            get { return _Game ?? (_Game = new MinesweeperGame()); }
+        }
+        private void Game_Tick(int time)
+        {
+            Debug.WriteLine("Time: " + time);
+            IView curView = ViewManager.Instance.CurrentView as GameView;
+            if (curView == null)
+                return;
+
+            //todoL update counter
+        }
+
+        private void Game_GameOver(bool isWin)
+        {
+            Console.WriteLine(String.Format("Game over: You {0}!", isWin ? "win" : "lose"));
+        }
+
+        private void Game_CellStateChange(int x, int y)
+        {
+            Cell cell = _Game.Cell(x, y);
+            Debug.WriteLine(String.Format("Game_CellStateChange: new state is {0}.", cell.State));
+        }
+
+        Options _Options = null;
+        Menu _Menu = null;
+        MinesweeperConsoleApp()
+        {
+            _Menu = new Menu();
+            _Options = Options.Beginner;
+            _Game = new MinesweeperGame();
+            _Game.CellStateChange += Game_CellStateChange;
+            _Game.GameOver += Game_GameOver;
+            _Game.Tick += Game_Tick;
+            ViewManager.Instance.Add(new GameView());
+            _Game.New();
+        }
+
+
+        bool _NeedClearScreen = true;
+
+        void draw()
+        {
+            IView curView = ViewManager.Instance.CurrentView;
+            if (_NeedClearScreen)
+            {
+                Console.Clear();
+                _Menu.Draw();
+            }
+            curView.Draw();
+
+        }
+        public bool Run()
+        {
+            IView curView = ViewManager.Instance.CurrentView;
+            if (curView == null)
+                return false;
+            draw();
+            string userInput = Console.ReadLine();
+            if (!_Menu.ProcessUserInput(userInput))
+                _NeedClearScreen = curView.ProcessUserInput(userInput);
+            return true;
+        }
+    }
+}
